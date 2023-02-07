16df17cb9366963eff9940fbe6ba09ff1f103c6e
vojcc <kuba.wojcik000@gmail.com>
2023-02-07 22:35:55 +0100

    fix navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,8 +5,8 @@
         <img alt="" src="https://img.icons8.com/windows/32/676767/plus-math--v1.png"/>
       </router-link>
 
-      <router-link :to="{name: 'home'}" class="px-4 py-3">
-        <img v-if="$route.path !== '/'" alt="" src="https://img.icons8.com/ios/32/null/sheet-music.png"/>
+      <router-link v-if="$route.path !== '/'" :to="{name: 'home'}" class="px-4 py-3">
+        <img alt="" src="https://img.icons8.com/ios/32/null/sheet-music.png"/>
       </router-link>
     </div>
   </nav>
